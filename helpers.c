fc65d01a204fa8d7afda3b6e706b788d8f8d9a95
AlexWei704 <143023270+AlexWei704@users.noreply.github.com>
2024-01-08 14:16:46 -0800

    helpers.c

--- helpers.c
+++ helpers.c
@@ -1,15 +1,20 @@
 #include "helpers.h"
+#include <stdlib.h>
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
+        int blue = rand() % 256;
+        int red = rand() % 256;
+        int green = rand() % 256;
         for (int j = 0; j < width; j++)
         {
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
-                image[i][j].rgbtBlue = 0xff;
-                image[i][j].rgbtRed = 0xff;
+                image[i][j].rgbtBlue = blue;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtRed = green;
             }
         }
     }
